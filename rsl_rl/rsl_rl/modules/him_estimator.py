5bbc39e2a5b9b1eb553a7507d4aa148ab8671efd
toonasinesis <799946523@qq.com>
2024-11-28 19:36:44 +0800

    change decoder

--- rsl_rl/rsl_rl/modules/him_estimator.py
+++ rsl_rl/rsl_rl/modules/him_estimator.py
@@ -40,13 +40,29 @@ class HIMEstimator(nn.Module):
                                             nn.ELU()))
         for l in range(len(decoder_hidden_dims)):
             if l == len(decoder_hidden_dims) - 1:
-                decoder_layers.append(nn.Linear(decoder_hidden_dims[l], num_one_step_obs+187))
+                decoder_layers.append(nn.Linear(decoder_hidden_dims[l], num_one_step_obs))
             else:
                 decoder_layers.append(nn.Sequential(nn.Linear(decoder_hidden_dims[l], decoder_hidden_dims[l+1]),
                                         nn.BatchNorm1d(decoder_hidden_dims[l+1]),
                                         nn.ELU()))
-        self.decoder = nn.Sequential(*decoder_layers)        
-        print("decoder",self.decoder)
+        self.obs_decoder = nn.Sequential(*decoder_layers)        
+        print("obs_decoder",self.obs_decoder)
+
+
+        height_decoder_layers = []
+        height_decoder_input_dim = 32
+        height_decoder_layers.append(nn.Sequential(nn.Linear(height_decoder_input_dim, decoder_hidden_dims[0]),
+                                            nn.BatchNorm1d(decoder_hidden_dims[0]),
+                                            nn.ELU()))
+        for l in range(len(decoder_hidden_dims)):
+            if l == len(decoder_hidden_dims) - 1:
+                height_decoder_layers.append(nn.Linear(decoder_hidden_dims[l], 187))
+            else:
+                height_decoder_layers.append(nn.Sequential(nn.Linear(decoder_hidden_dims[l], decoder_hidden_dims[l+1]),
+                                        nn.BatchNorm1d(decoder_hidden_dims[l+1]),
+                                        nn.ELU()))
+        self.height_decoder = nn.Sequential(*height_decoder_layers)        
+        print("height_decoder",self.height_decoder)
 
         #HISTORY with height Encoder 
         encoder_layers = []
@@ -55,7 +71,6 @@ class HIMEstimator(nn.Module):
                                             nn.BatchNorm1d(encoder_hidden_dims[0]),
                                             nn.ELU()))
 
-        encoder_input_dim = self.temporal_steps * self.num_one_step_obs
         for l in range(len(encoder_hidden_dims) - 1):
             encoder_layers.append(nn.Sequential(nn.Linear(encoder_hidden_dims[l], encoder_hidden_dims[l+1]),
                             nn.BatchNorm1d(encoder_hidden_dims[l+1]),
@@ -63,8 +78,6 @@ class HIMEstimator(nn.Module):
         self.encoder = nn.Sequential(*encoder_layers)
 
 
-
-        
 
         self.fc_mu = nn.Linear(encoder_hidden_dims[-1], latent_dim)
         self.fc_var = nn.Linear(encoder_hidden_dims[-1], latent_dim)
@@ -97,8 +110,9 @@ class HIMEstimator(nn.Module):
     
     def decode(self, z, v):
         input = torch.cat([z, v], dim = 1)
-        output = self.decoder(input)
-        return output
+        obs_output = self.obs_decoder(input)
+        height_output = self.height_decoder(z[:,-32:])
+        return obs_output,height_output
 
     def encode(self, obs_history):
         encoded = self.encoder(obs_history.detach())
@@ -116,15 +130,16 @@ class HIMEstimator(nn.Module):
                 
         vel = next_critic_obs[:, self.num_one_step_obs:self.num_one_step_obs+3].detach()
         next_obs = next_critic_obs.detach()[:, 3:self.num_one_step_obs+3]
-        privi_height = next_critic_obs.detach()[:,self.num_one_step_obs+3+3:self.num_one_step_obs+3+3+187]
+        privi_height = obs_history.detach()[:,-187:]
 
         vel_pred, z, latent_mu, latent_logvar = self.encode(obs_history)
         # z_t = self.target(next_obs)
         # pred_vel, z_s = z_s[..., :3], z_s[..., 3:]
         estimation_loss = F.mse_loss(vel_pred, vel)
-        obs_next_pred =self.decode(z, vel_pred)
-        reconstruct_loss = F.mse_loss(obs_next_pred[:,:self.num_one_step_obs], next_obs)\
-        + F.mse_loss(obs_next_pred[:,self.num_one_step_obs:self.num_one_step_obs+187],privi_height)
+        obs_next_pred ,height_pred=self.decode(z, vel_pred)
+        reconstruct_loss = F.mse_loss(obs_next_pred[:,:self.num_one_step_obs], next_obs)+\
+        F.mse_loss(height_pred,privi_height)
+       
 
         kld_loss = -0.5 * torch.sum(1 + latent_logvar - latent_mu ** 2 - latent_logvar.exp(), dim = 1)
         kld_loss_mean = kld_loss.mean()
